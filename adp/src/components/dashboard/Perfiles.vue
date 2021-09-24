69121221794cb43e0b915a4b63919c4fa3325c1d
yerigagarin <yersonolivares@protonmail.com>
2021-09-24 14:45:34 -0300

    Mejora en cards

--- adp/src/components/dashboard/Perfiles.vue
+++ adp/src/components/dashboard/Perfiles.vue
@@ -21,17 +21,24 @@
       </b-col>
     </b-row>
     <!-- Contenido -->
-    <b-card no-body class="overflow-hidden">
+    <b-card
+      no-body
+      class="overflow-hidden"
+      v-for="(adp, i) in filtrarADPs"
+      :key="i"
+    >
       <b-row no-gutters>
         <b-col md="6" class="text-start">
           <b-card-body title="Identificación">
-            <ul v-for="(adp, i) in filtrarADPs" :key="i">
+            <ul>
               <li>
-                Nombre: {{ adp.nombre_corregido }} {{ adp.apellido_corregido }}
+                Nombre: {{ adp.nombre_corregido }}
+                {{ adp.apellido_corregido }}
               </li>
               <li>{{ i + 1 }} {{ adp.concurso }}</li>
               <li>
-                Fecha de nombramiento: {{ adp.fecha_nombramiento_renovacion }}
+                Fecha de nombramiento:
+                {{ adp.fecha_nombramiento_renovacion | formatDate }}
               </li>
               <li>Estado en el sistema: {{ adp.estado_adp }}</li>
               <li>Estado de convenio: {{ adp.estado_cd }}</li>
@@ -46,23 +53,24 @@
           </b-card-body>
         </b-col>
         <b-col md="6">
-          <b-card-body title="Horizontal Card">
-            <ul v-for="(adp, i) in filtrarADPs" :key="i">
+          <b-card-body title="Fechas">
+            <ul>
               <li>
-                Fecha de nombramiento: {{ adp.fecha_nombramiento_renovacion }}
+                Fecha de nombramiento:
+                {{ adp.fecha_nombramiento_renovacion | formatDate }}
               </li>
-              <li>30 días: {{ adp.fecha_propuesta }}</li>
-              <li>60 días: {{ adp.fecha_suscripcion }}</li>
-              <li>90 días: {{ adp.fecha_comunicacion }}</li>
+              <li>30 días: {{ adp.fecha_propuesta | formatDate }}</li>
+              <li>60 días: {{ adp.fecha_suscripcion | formatDate }}</li>
+              <li>90 días: {{ adp.fecha_comunicacion | formatDate }}</li>
               <hr />
-              <li>{{ adp.eval_semestral_inicio }}</li>
-              <li>{{ adp.eval_semestral_auto }}</li>
-              <li>{{ adp.eval_semestral_retro }}</li>
+              <li>{{ adp.eval_semestral_inicio | formatDate }}</li>
+              <li>{{ adp.eval_semestral_auto | formatDate }}</li>
+              <li>{{ adp.eval_semestral_retro | formatDate }}</li>
               <hr />
-              <li>{{ adp.eval_anual_inicio }}</li>
-              <li>{{ adp.eval_anual_auto }}</li>
-              <li>{{ adp.eval_anual_retro }}</li>
-              <li>{{ adp.eval_anual_rex }}</li>
+              <li>{{ adp.eval_anual_inicio | formatDate }}</li>
+              <li>{{ adp.eval_anual_auto | formatDate }}</li>
+              <li>{{ adp.eval_anual_retro | formatDate }}</li>
+              <li>{{ adp.eval_anual_rex | formatDate }}</li>
             </ul>
           </b-card-body>
         </b-col>
@@ -111,21 +119,32 @@ export default {
   },
   methods: {
     filtroPorNombreDeServicio(adps) {
-      return adps.filter((product) => !product.servicio.indexOf(this.servicio));
+      return adps.filter((adp) => !adp.servicio.indexOf(this.servicio));
     },
     filtroPorConcurso(adps) {
-      return adps.filter((product) => !product.concurso.indexOf(this.concurso));
+      return adps.filter((adp) => !adp.concurso.indexOf(this.concurso));
     },
   },
   computed: {
     filtrarADPs() {
       return this.filtroPorConcurso(this.filtroPorNombreDeServicio(this.adps));
     },
+    concursoMasActual() {
+      const arregloConcursos = this.adps;
+      return Math.max.apply(
+        Math,
+        arregloConcursos.map((n) => {
+          return n.concurso;
+        })
+      );
+    },
     ...mapState(["adps"]),
+  },
+  created() {
+    this.concurso = this.concursoMasActual;
   },
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 </style>
