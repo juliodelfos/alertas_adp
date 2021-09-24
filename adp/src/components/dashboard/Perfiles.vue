cd5287576d414cf851393a18c14ac9bc94b3100f
yerigagarin <yersonolivares@protonmail.com>
2021-09-24 11:40:37 -0300

    Filtros funcionando

--- adp/src/components/dashboard/Perfiles.vue
+++ adp/src/components/dashboard/Perfiles.vue
@@ -3,7 +3,7 @@
     <!-- Buscador -->
     <b-row cols="1" cols-sm="1" cols-md="2" cols-lg="2">
       <b-col>
-        <b-form-group label="Buscar por nombre de concurso:">
+        <!-- <b-form-group label="Buscar por nombre de concurso:">
           <b-form-input
             placeholder="nombre"
             v-model="concurso"
@@ -17,20 +17,45 @@
             class="form-select"
           >
           </b-form-select>
-        </b-form-group>
+        </b-form-group> -->
       </b-col>
     </b-row>
     <!-- Contenido -->
     <b-card no-body class="overflow-hidden">
       <b-row no-gutters>
         <b-col md="6" class="text-start">
-          <b-card-body title="Identificación">
+          <select v-model="servicio">
+            <option value="">Elige categoría</option>
+            <option value="Servicio Local de Educación Andalién Sur">
+              Servicio Local de Educación Andalién Sur
+            </option>
+            <option value="Superintendencia de Educación Superior">
+              Superintendencia de Educación Superior
+            </option>
+            <option value="Servicio Local de Educación Barrancas">
+              Servicio Local de Educación Barrancas
+            </option>
+            <option value="Consejo de Rectores">Consejo de Rectores</option>
+          </select>
+
+          <input
+            type="text"
+            v-model="concurso"
+            placeholder="Filtrar por número de concurso"
+          />
+
+          <ul>
+            <li v-for="(product, i) in filtrarADPs" :key="i">
+              ADP: {{ product.concurso }} -{{ product.servicio }}
+            </li>
+          </ul>
+          <!-- <b-card-body title="Identificación">
             <ul v-for="({ concurso }, i) in adps" :key="i">
-              <!-- <li>
+              <li>
                 Nombre: {{ adp.nombre_corregido }} {{ adp.apellido_corregido }}
-              </li> -->
+              </li>
               <li>{{ i + 1 }} {{ concurso }}</li>
-              <!-- <li>
+              <li>
                 Fecha de nombramiento: {{ adp.fecha_nombramiento_renovacion }}
               </li>
               <li>Estado en el sistema: {{ adp.estado_adp }}</li>
@@ -41,12 +66,12 @@
               <li>{{ adp.region }}</li>
               <li>{{ adp.mail }}</li>
               <li>{{ adp.mail_contraparte_cd }}</li>
-              <li>{{ adp.mail_contraparte_eval }}</li> -->
+              <li>{{ adp.mail_contraparte_eval }}</li>
             </ul>
-          </b-card-body>
+          </b-card-body> -->
         </b-col>
-        <!-- <b-col md="6">
-          <b-card-body title="Horizontal Card">
+        <b-col md="6">
+          <!-- <b-card-body title="Horizontal Card">
             <ul v-for="(adp, i) in adps" :key="i">
               <li>
                 Fecha de nombramiento: {{ adp.fecha_nombramiento_renovacion }}
@@ -64,8 +89,8 @@
               <li>{{ adp.eval_anual_retro }}</li>
               <li>{{ adp.eval_anual_rex }}</li>
             </ul>
-          </b-card-body>
-        </b-col> -->
+          </b-card-body> -->
+        </b-col>
       </b-row>
     </b-card>
   </b-container>
@@ -78,47 +103,98 @@ export default {
   name: "Perfiles",
   data() {
     return {
-      concurso: "",
       servicio: "",
-      servicios: [
-        { value: "", text: "Seleccione una opción" },
-        {
-          value: "Servicio Local de Educación Andalién Sur",
-          text: "Servicio Local de Educación Andalién Sur",
-        },
-        {
-          value: "Superintendencia de Educación Superior",
-          text: "Superintendencia de Educación Superior",
-        },
-        {
-          value: "Servicio Local de Educación Barrancas",
-          text: "Servicio Local de Educación Barrancas",
-        },
-        {
-          value: "Servicio Local de Educación Colchagua",
-          text: "Servicio Local de Educación Colchagua",
-        },
-        { value: "Consejo de Rectores", text: "Consejo de Rectores" },
-        {
-          value: "Servicio Nacional de Educación Pública",
-          text: "Dirección de Educación Pública",
-        },
-      ],
+      concurso: "",
+      // products: [
+      //   {
+      //     concurso: "2113",
+      //     servicio: "Servicio Local de Educación Andalién Sur",
+      //     alerta0: "null",
+      //     nombre_corregido: "Juan Rafael",
+      //     apellido_corregido: "Bravo Miranda",
+      //     alerta_cero: "No",
+      //   },
+      //   {
+      //     concurso: "2040",
+      //     servicio: "Consejo de Rectores",
+      //     alerta0: "null",
+      //     nombre_corregido: "Juan Rafael",
+      //     apellido_corregido: "Bravo Miranda",
+      //     alerta_cero: "No",
+      //   },
+      //   {
+      //     concurso: "3682",
+      //     servicio: "Servicio Local de Educación Andalién Sur",
+      //   },
+      //   {
+      //     concurso: "3947",
+      //     servicio: "Servicio Local de Educación Barrancas",
+      //   },
+      //   {
+      //     concurso: "3901",
+      //     servicio: "Servicio Local de Educación Barrancas",
+      //   },
+      //   {
+      //     concurso: "4019",
+      //     servicio: "Superintendencia de Educación Superior",
+      //   },
+      //   {
+      //     concurso: "3951",
+      //     servicio: "Superintendencia de Educación Superior",
+      //   },
+      //   { concurso: "4214", servicio: "Consejo de Rectores" },
+      //   {
+      //     concurso: "4131",
+      //     servicio: "Superintendencia de Educación Superior",
+      //   },
+      //   { concurso: "4064", servicio: "Consejo de Rectores" },
+      // ],
+      // servicios: [
+      //   { value: "", text: "Seleccione una opción", price: 100 },
+      //   {
+      //     value: "Servicio Local de Educación Andalién Sur",
+      //     text: "Servicio Local de Educación Andalién Sur",
+      //     price: 120,
+      //   },
+      //   {
+      //     value: "Superintendencia de Educación Superior",
+      //     text: "Superintendencia de Educación Superior",
+      //     price: 200,
+      //   },
+      //   {
+      //     value: "Servicio Local de Educación Barrancas",
+      //     text: "Servicio Local de Educación Barrancas",
+      //     price: 400,
+      //   },
+      //   {
+      //     value: "Servicio Local de Educación Colchagua",
+      //     text: "Servicio Local de Educación Colchagua",
+      //     price: 500,
+      //   },
+      //   {
+      //     value: "Consejo de Rectores",
+      //     text: "Consejo de Rectores",
+      //     price: 600,
+      //   },
+      //   {
+      //     value: "Servicio Nacional de Educación Pública",
+      //     text: "Dirección de Educación Pública",
+      //     price: 700,
+      //   },
+      // ],
     };
   },
   methods: {
-    filtrarPorCategoria(adps) {
-      return adps.filter((e) => !e.servicio.indexOf(this.servicio));
+    filtroPorNombreDeServicio: function (adps) {
+      return adps.filter((product) => !product.servicio.indexOf(this.servicio));
     },
-    filtrarPorNombre(adps) {
-      return adps.filter(
-        (e) => !e.concurso.toLowerCase().indexOf(this.concurso.toLowerCase())
-      );
+    filtroPorConcurso: function (adps) {
+      return adps.filter((product) => !product.concurso.indexOf(this.concurso));
     },
   },
   computed: {
-    filtros() {
-      return this.filtrarPorCategoria(this.filtrarPorNombre(this.adps));
+    filtrarADPs: function () {
+      return this.filtroPorConcurso(this.filtroPorNombreDeServicio(this.adps));
     },
     ...mapState(["adps"]),
   },
