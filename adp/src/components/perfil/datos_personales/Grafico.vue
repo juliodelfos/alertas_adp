85caf717c160d3c1c8081852a33523831aa9450e
Yerson <yersonolivares@protonmail.com>
2021-11-15 10:28:39 -0300

    Se añade gráfico para valores 0
    Cuando fecha es futura, se muestra gráficos con avance en 0%

--- adp/src/components/perfil/datos_personales/Grafico.vue
+++ adp/src/components/perfil/datos_personales/Grafico.vue
@@ -1,22 +1,43 @@
 <template>
-  <b-container class="pt-4" v-show="porcentaje_dias_cargo !== 'Fecha futura'">
-    <h3 class="fs-6 text-start">Porcentaje avance año de gestión</h3>
-    <b-progress
-      :value="porcentaje_dias_anogestion"
-      class="mb-3"
-      height="20px"
-      variant="primary"
-      show-progress
-    ></b-progress>
+  <div>
+    <b-container class="pt-4" v-if="porcentaje_dias_cargo !== 'Fecha futura'">
+      <h3 class="fs-6 text-start">Porcentaje avance año de gestión</h3>
+      <b-progress
+        :value="porcentaje_dias_anogestion"
+        class="mb-3"
+        height="20px"
+        variant="primary"
+        show-progress
+      ></b-progress>
 
-    <h3 class="fs-6 text-start">Porcentaje avance periodo</h3>
-    <b-progress
-      :value="porcentaje_dias_cargo"
-      height="20px"
-      show-progress
-      variant="success"
-    ></b-progress>
-  </b-container>
+      <h3 class="fs-6 text-start">Porcentaje avance periodo</h3>
+      <b-progress
+        :value="porcentaje_dias_cargo"
+        height="20px"
+        show-progress
+        variant="success"
+      ></b-progress>
+    </b-container>
+
+    <b-container class="pt-4" v-else>
+      <h3 class="fs-6 text-start">Porcentaje avance año de gestión</h3>
+      <b-progress
+        value="0"
+        class="mb-3"
+        height="20px"
+        variant="primary"
+        show-progress
+      ></b-progress>
+
+      <h3 class="fs-6 text-start">Porcentaje avance periodo</h3>
+      <b-progress
+        value="0"
+        height="20px"
+        show-progress
+        variant="success"
+      ></b-progress>
+    </b-container>
+  </div>
 </template>
 
 <script>
